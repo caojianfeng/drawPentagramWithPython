0ee4c47ad527cbae127a850f1e29d7d9d5b8f346
曹诺一 <caonuoyi@caojianfengs-MacBook-Pro.local>
2019-02-17 15:40:09 +0800

    style

--- t03_pentagram_storm.py
+++ t03_pentagram_storm.py
@@ -2,13 +2,12 @@ import math
 from turtle import *
 from draw_pentagram import draw_pentagram
 
-edge_count = 5
-angle = 144
 block_angle = 288
 init_size = 200
 # scale = (4- math.sqrt(5))/2
-# scale = (math.sqrt(5)-1)/2
-scale = math.pow((math.sqrt(5)-1)/2, 1/5)
+scale = (math.sqrt(5)-1)/2
+# scale = math.pow((math.sqrt(5)-1)/2, 1/5)
+print(scale)
 
 speed(0)
 setpos(0, 0)
@@ -18,7 +17,7 @@ hideturtle()
 def draw_next_pentagram(size):
     new_size = scale*size
     right(block_angle)
-    if new_size > 10:
+    if new_size > 3:
         draw_pentagram(size)
         draw_next_pentagram(new_size)
 
